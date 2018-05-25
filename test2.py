d6206c731caef6ae74d7ee53250ded61ed858c51
RasElasedAustralis <mmoebis@gmx.de>
2018-05-25 22:22:21 +0200

    Delete test2.py

--- test2.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-ex = np.linspace(0, 2 * np.pi, 50, endpoint=True)
-F = np.sin(ex)
-plt.plot(ex,F)
-startx, endx = -0.1, 2*np.pi + 0.1
-starty, endy = -1.1, 1.1
-plt.axis([startx, endx, starty, endy])
-plt.show()
